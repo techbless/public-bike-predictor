15751d9b97edc04b8413f160bc1f249ac5d8db02
SongHH123 <1971318@hansung.ac.kr>
2020-08-03 21:28:07 +0900

    Show station cards in one line & color for forecast result

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,23 +3,6 @@ import Card from './Card';
 import DetailModal from './DetailModal';
 import './App.css';
 
-function chunks(arr, size) {
-  if (!Array.isArray(arr)) {
-    throw new TypeError('Input should be Array');
-  }
-
-  if (typeof size !== 'number') {
-    throw new TypeError('Size should be a Number');
-  }
-
-  let result = [];
-  for(let i = 0; i < arr.length; i += size) {
-    result.push(arr.slice(i, size + i));
-  }
-
-  return result;
-}
-
 function App() {
 
   const [stations, setStations] = useState([]);
@@ -27,17 +10,12 @@ function App() {
 
   useEffect(() => {
     async function fetchData() {
-      const APIURL = "http://openapi.seoul.go.kr:8088/664347526363796238356265787544/json/bikeList/1/1000"
+      const APIURL = "http://49.50.166.60:5000/stations/available"
       const resultInJson = await fetch(APIURL);
       const result = await resultInJson.json();
 
-      if (result.rentBikeStatus.RESULT.CODE !== 'INFO-000') {
-        return alert("에러가 발생했습니다.");
-      }
-
-      setStations(result.rentBikeStatus.row);
-
-      setRows(chunks(result.rentBikeStatus.row, 2));
+      setStations(result);
+      // setRows(chunks(result, 2));
     }
     
     fetchData();
@@ -47,26 +25,27 @@ function App() {
     <div className="App container-fluid">
       <DetailModal show={true}/>
       <div className="row">
-        <div className="col-md-6 bg-success App_map">
+
+        <div className="col-md-9 bg-success App_map">
           <h1>Map will be here.</h1>
         </div>
-        <div className="col-md-6 App_station_list">
-          <div class="container mt-5">
-          {
-            rows.map((row) => (
-              <div className="row">
-                {row.map((col) => (
+
+        <div className="col-md-3 App_station_list">
+          <div className="container mt-5">
+            {
+              stations.map((station) => (
                   <Card
-                    name = {col.stationName}
-                    id = {col.stationId}
-                    stock = {col.parkingBikeTotCnt}
+                    name = {station.stationName}
+                    id = {station.stationId}
+                    stock = {station.parkingBikeTotCnt}
+                    future = {station.future}
                   />
-                ))}
-              </div>
-            ))
-          }
+              ))
+            }
           </div>
         </div>
+
+        
       </div>
     </div>
   );
--- frontend/src/Card.js
+++ frontend/src/Card.js
@@ -1,17 +1,23 @@
 import React, { useState, useEffect, Button } from 'react';
 import './Card.css';
-import DetailModel from './DetailModal';
 
+function mean(arr) {
+    return arr.reduce((a, b) => a + b) / arr.length;
+}
 
 function Card(props) {
 
     return (
         <div
-            className="Card_card col-md-6 my-2 shadow-sm rounded"
+            className={
+                mean(props.future) >= props.stock ? 
+                "up Card_card my-2 shadow-sm rounded" : 
+                "down Card_card my-2 shadow-sm rounded"
+            }
         >
             <h4>{props.name}</h4>
             <hr />
-            <h5>{props.id} - {props.stock}</h5>
+            <h5>현재: {props.stock}, 예측: {mean(props.future)}</h5>
         </div>
     );
     
